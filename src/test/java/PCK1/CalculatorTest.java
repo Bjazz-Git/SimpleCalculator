c2c929e538ac71b0816fa495113aa4ab7446d3a8
Bjazz <braxtonjazz05@gmail.com>
2025-06-22 13:06:00 -0400

    changes

--- src/test/java/PCK1/CalculatorTest.java
+++ src/test/java/PCK1/CalculatorTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class CalculatorTest {
 	int testNumber1 = (int) (Math.random() * 1000) + 1;
-	int testNumber2 = 10;
+	int testNumber2 = (int) (Math.random() * 1000) + 1;
 	
     @Test
 	public void testAddition() {
