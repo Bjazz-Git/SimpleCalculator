749c6714d06e49fd41105259a1e0b2510df57475
Bjazz <braxtonjazz05@gmail.com>
2025-08-03 20:11:36 -0400

    remove sleep()

--- src/main/java/PCK1/MainClass.java
+++ src/main/java/PCK1/MainClass.java
@@ -135,11 +135,11 @@ public class MainClass {
 	//Method that handles addition
 	public static int addNumber(int number1, int number2) {
 		int result = number1 + number2;
-		try{
-			Thread.sleep(10000);
-		}catch(Exception ex){
+		// try{
+		// 	Thread.sleep(10000);
+		// }catch(Exception ex){
 
-		}
+		// }
 		return result;
 	}
 	
